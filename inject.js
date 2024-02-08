c3d77abd04cc51781a9b9a9ded398e81f5b97147
Nacho-Cola <sugy1030@gmail.com>
2024-02-09 00:20:15 +0900

    commit

--- inject.js
+++ inject.js
@@ -8,3 +8,26 @@
 // })
 // }, 500)
 
+
+const observer = new MutationObserver((mutationsList, observer) => {
+  for(let mutation of mutationsList) {
+    if (mutation.type === 'childList') {
+      let container = document.getElementById("secondary-inner");
+      if (container) {
+        addElement(container);
+        observer.disconnect();
+        break;
+      }
+    }
+  }
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
+
+function addElement (container) {
+  var hi_button = document.createElement('div');
+  hi_button.style.backgroundColor = 'white';
+  hi_button.style.height = '50px'
+  hi_button.innerHTML = '<b>Youtube AI</b>';
+  container.prepend(hi_button);
+}

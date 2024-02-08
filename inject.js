2ec2fcd69bb5258cfc5822ef1c95c7da22be2537
Nacho-Cola <sugy1030@gmail.com>
2024-02-08 18:13:48 +0900

    commit

---
+++ inject.js
@@ -0,0 +1,10 @@
+
+
+// setInterval(()=>{
+
+// let imgs = document.querySelectorAll('img');
+// imgs.forEach((a,i)=>{
+//   a.src = 'https://lh3.googleusercontent.com/proxy/a5L0o0spfVJWmrrn13av-FPd53ZfSyUQ3F3Yh_jXWWGnXqT7KLnt6hqX4wx0JzPMqYOXxsqtwKK-3LQua6hrlAGLD9OA_v3Gtm6Lx9qdN8w09qQiHa-iA-hGY598t8zAeiyohueop7Z_d9k-xMvEuG7TN591woiBQ-3YHmoRrcneurQbk7m_T8I'
+// })
+// }, 500)
+
